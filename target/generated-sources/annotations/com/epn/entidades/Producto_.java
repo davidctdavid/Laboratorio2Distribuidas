49b4dd8e5dfdb1ce34b3767ac20cc3fd89116a08
Aplicaciones D <Aplicaciones D@1-21-01-MD-25>
2019-10-31 16:46:31 -0500

    update

---
+++ target/generated-sources/annotations/com/epn/entidades/Producto_.java
@@ -0,0 +1,16 @@
+package com.epn.entidades;
+
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-31T16:24:52")
+@StaticMetamodel(Producto.class)
+public class Producto_ { 
+
+    public static volatile SingularAttribute<Producto, Integer> codigo;
+    public static volatile SingularAttribute<Producto, BigDecimal> precio;
+    public static volatile SingularAttribute<Producto, String> nombre;
+
+}
